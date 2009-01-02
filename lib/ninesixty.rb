3beb5cbe8db882e47a4347ffbe331ea5f5a10d68
Chris Eppstein <chris@eppsteins.net>
2009-01-01 22:08:48 -0800

    fixed require statements.

--- lib/ninesixty.rb
+++ lib/ninesixty.rb
@@ -1,2 +1,2 @@
-require File.join(File.dirname(__FILE__),'compass_plugin')
-require File.join(File.dirname(__FILE__),'sass_extensions')
+require File.join(File.dirname(__FILE__), 'ninesixty', 'compass_plugin')
+require File.join(File.dirname(__FILE__), 'ninesixty', 'sass_extensions')
