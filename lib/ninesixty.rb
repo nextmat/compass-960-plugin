b04d167b30cce50e6dfa875d423b05412a962f1e
Chris Eppstein <chris@eppsteins.net>
2009-09-27 13:07:00 -0700

    The enumerate method is now provided by compass and not needed here.

--- lib/ninesixty.rb
+++ lib/ninesixty.rb
@@ -1,2 +1,1 @@
 require File.join(File.dirname(__FILE__), 'ninesixty', 'compass_plugin')
-require File.join(File.dirname(__FILE__), 'ninesixty', 'sass_extensions')
--- lib/ninesixty/sass_extensions.rb
+++
@@ -1,7 +0,0 @@
-require 'sass'
-
-module Sass::Script::Functions
-  def enumerate(prefix, from_index, to_index)
-    Sass::Script::String.new(((from_index.value)..(to_index.value)).map{|i| "#{prefix}#{i}"}.join(", "))
-  end
-end
